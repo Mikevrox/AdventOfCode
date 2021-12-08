bc780982c496a29cf3494e19f2f5bd0a4d1971d6
Mikevrox <mikedevstudio@gmail.com>
2021-12-08 17:59:32 +0100

    Day One first exercise with user input

--- src/com/advent/code/dayone/Main.java
+++ src/com/advent/code/dayone/Main.java
@@ -1,5 +1,10 @@
 package com.advent.code.dayone;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     /*
@@ -26,6 +31,41 @@ public class Main {
 
     public static void main(String[] args){
 
+        //Create List from console input
+        try{
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
+            List<Long> measurements = new ArrayList<>();
+            boolean endInput = false;
+
+            while(!endInput){
+                System.out.println("Please provide a measurement as a number with no decimals: ");
+                String measurementFromInput = bufferedReader.readLine();
+                try{
+                    measurements.add(Long.parseLong(measurementFromInput));
+                }catch(NumberFormatException e){
+                    System.out.println("The provided measurement is not a valid number.");
+                    continue;
+                }
+                System.out.println("Do you want to continue? Y/N");
+                String insertAgain = bufferedReader.readLine();
+                if(insertAgain != null &&
+                        insertAgain.equalsIgnoreCase("N")){
+                    endInput = true;
+                }
+            }
+
+            Long increasedMeasurements = 0L;
+            for(int i = 1; i< measurements.size(); i++){
+                Long measurement = measurements.get(i);
+                if(measurement > measurements.get(i - 1)){
+                    increasedMeasurements += 1;
+                }
+            }
+
+            System.out.println("The number of increased measurements is: " + increasedMeasurements);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
