7205f2aa1f40740f6294019d8b3232df381f7e6d
Mikevrox <mikedevstudio@gmail.com>
2021-12-08 17:29:01 +0100

    Initial Commit - Day one

---
+++ src/com/advent/code/dayone/Main.java
@@ -0,0 +1,31 @@
+package com.advent.code.dayone;
+
+public class Main {
+
+    /*
+        Example data
+        199
+        200
+        208
+        210
+        200
+        207
+        240
+        269
+        260
+        263
+
+        This is the depths scanned by the sonar.
+        First, count the number of times a depth measurement increases from the previous measurement.
+        First measurement has nothing before it.
+
+        Quiz question.
+        How many measurements are larger than the previous measurement?
+     */
+
+
+    public static void main(String[] args){
+
+
+    }
+}
